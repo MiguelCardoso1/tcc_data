8581ce14598db9769965b542d91df9568ba3436d
Miguel Malton <miguel.malton@gmail.com>
2025-11-03 20:42:04 -0300

    Api e importação do modelo

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+import os
+from pydantic import BaseModel
+
+# Inicializa a API
+app = FastAPI()
+
+class InputData(BaseModel):
+    features : list
+
+# Carrega o modelo salvo
+modelo = joblib.load("modelo.pkl")
+
+@app.get("/")
+def home():
+    return {"message": "API Decision Tree rodando!"}
+
+@app.post("/predict")
+def predict(data : InputData):
+    arr = np.array(data.features).reshape(1, -1)
+    pred = modelo.predict(arr)
+    return {"prediction": int(pred[0])}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
